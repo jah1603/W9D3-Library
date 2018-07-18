6593663b0d58f1f30b1b4e1daf76044329a321b7
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-18 13:29:11 +0100

    borrower getters and setters

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,4 +1,53 @@
 package models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "borrowers")
+
 public class Borrower {
+
+    private int id;
+    private String name;
+    private List<Book> collection;
+
+    public Borrower(String name) {
+        this.name = name;
+        this.collection = new ArrayList<Book>();
+    }
+
+    public Borrower() {
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    public List<Book> getCollection() {
+        return collection;
+    }
+
+    public void setCollection(List<Book> collection) {
+        this.collection = collection;
+    }
 }
