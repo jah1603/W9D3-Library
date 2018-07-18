f0ba51246ed3274e5479f826aeab34acc51d35d0
James Henderson <hendersonjamesalexander@gmail.com>
2018-07-18 13:20:59 +0100

    genres enum

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,4 +1,17 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="books")
 public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean onLoan;
+    private Borrower currentBorrower;
+    private enum GENRE;
+
 }
--- src/main/java/models/Genre.java
+++ src/main/java/models/Genre.java
@@ -1,4 +1,19 @@
 package models;
 
 public enum Genre {
+
+    HISTORY,
+    POLITICS,
+    SPORT,
+    ROMANCE,
+    TEEN,
+    LITERARY,
+    MUSIC,
+    HEALTH,
+    LIFESTYLE,
+    AUTOBIOGRAPHY;
+
+
+
+
 }
